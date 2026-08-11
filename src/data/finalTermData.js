6a0bfb47097241be931988d283905c66bef910bc
Sahirullah <sahirullah313@gmail.com>
2026-08-11 09:07:15 +0500

    updaoitng

--- src/data/finalTermData.js
+++ src/data/finalTermData.js
@@ -427,6 +427,7 @@ export const finalTermDataPart2 = [
   { id: 60, code: 'CS310', title: 'Open Source Web Application Development', category: 'cs', image: '💻',Link:'' },
 //CS311  
   { id: 61, code: 'CS311', title: 'Introduction to Web Services Development', category: 'cs', image: '💻' ,Link:''},
+//CS312 
   { id: 62, code: 'CS312', title: 'Database Modeling and Design', category: 'cs', image: '💻',Link:'' },
   { id: 63, code: 'CS401', title: 'Computer Architecture & Assembly Language Programming', category: 'cs', image: '💻',links:[
     'https://drive.google.com/file/d/1yZ4ZDnnsLlH3pb515c6L2tuo38oUabgH/view?usp=sharing',
