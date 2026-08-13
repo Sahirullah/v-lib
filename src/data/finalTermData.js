6e80a4658288c89ba39cf4a9622d1a5d71ae55c0
Sahirullah <sahirullah313@gmail.com>
2026-08-13 07:57:24 +0500

    updating

--- src/data/finalTermData.js
+++ src/data/finalTermData.js
@@ -692,6 +692,7 @@ export const finalTermDataPart2 = [
   { id: 92, code: 'CS703', title: 'Advanced Operating Systems', category: 'cs', image: '💻',Link:'' },
   //CS704
   { id: 93, code: 'CS704', title: 'Advanced Computer Architecture II', category: 'cs', image: '💻',Link:'' },
+  //CS706
   { id: 94, code: 'CS706', title: 'Software Quality Assurance', category: 'cs', image: '💻',Link:'' },
   { id: 95, code: 'CS707', title: 'Network Security', category: 'cs', image: '💻',Link:'' },
   { id: 96, code: 'CS708', title: 'Software Requirement Engineering', category: 'cs', image: '💻',Link:'' },
