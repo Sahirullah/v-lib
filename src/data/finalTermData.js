9ec5caa50de248f91793e52114b6591386f06e21
Sahirullah <sahirullah313@gmail.com>
2026-08-14 12:05:53 +0500

    updating dropdown

--- src/data/finalTermData.js
+++ src/data/finalTermData.js
@@ -700,9 +700,9 @@ export const finalTermDataPart2 = [
   { id: 96, code: 'CS708', title: 'Software Requirement Engineering', category: 'cs', image: '💻',Link:'' },
   //CS710
   { id: 97, code: 'CS710', title: 'Mobile & Pervasive Computing', category: 'cs', image: '💻',Link:'' },
-  
+  //CS711
   { id: 98, code: 'CS711', title: 'Software Design', category: 'cs', image: '💻',Link:'' },
-  
+  //CS712
   { id: 99, code: 'CS712', title: 'Distributed DBMS', category: 'cs', image: '💻',Link:'' },
   
   { id: 100, code: 'CS718', title: 'Wireless Networks', category: 'cs', image: '💻',Link:'' },
